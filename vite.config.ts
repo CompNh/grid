1105392e59538dbb268a4eece1cd800f6354399d
CompNh <saramanda21c@naver.com>
2025-03-05 11:57:16 +0900

    스타일 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { copy } from "vite-plugin-copy";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    copy([{ src: "src/styles", dest: "dist" }]),    
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"), // ✅ 진입점을 index.ts로 지정
       name: "NHGridComponent", // ✅ 라이브러리 이름
       fileName: (format) =>
         format === "umd" ? "index.umd.js" : "index.es.js", // ✅ 파일명 변경
-    },
+    },    
     rollupOptions: {
       external: ["react", "react-dom"], // ✅ React를 번들에 포함하지 않음
       output: {
